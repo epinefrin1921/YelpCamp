d08ee09ebea09a35742a725e6599b312e2c45839
epinefrin1921 <nedim.ajdin@stu.ssst.edu.ba>
2020-11-13 15:47:26 +0100

    refactored style

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -1,11 +1,13 @@
 
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/dark-v10',
     center: [-103.59179687498357, 40.66995747013945],
     zoom: 3
 });
+
+map.addControl(new mapboxgl.NavigationControl())
 
 map.on('load', function () {
     // Add a new source from our GeoJSON data and
@@ -129,3 +131,4 @@ map.on('load', function () {
         map.getCanvas().style.cursor = '';
     });
 });
+
